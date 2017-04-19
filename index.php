754a1ed83da8715652295f7f0b816a8d9a95a9fd
niNeHertzz <glory.marcs@gmail.com>
2017-04-19 13:34:29 +0800

    Added login module

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<title>Simkimban</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+	</script>
+	<link href="contents/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+	<script type="text/javascript" src="contents/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="contents/user-access-style.css" />
+	<script type="text/javascript" src="scripts/login/windows-resize.js"></script>
+</head>
+<body class="transpa-body">
+	<!-- <div class="transpa-body"> -->
+		<div class="login-panel">
+			<div class="login-panel-body">
+				<div class="login-heading">
+					<img src="contents/images/simkimban_logo_10.png" height="90px" width="110" />
+				</div>
+				<div class="login-body">
+					<div class="form-group">
+						<input type="text" name="user" value="" class="form-control" placeholder="Username" style="opacity: .67;
+						color:#000" />
+					</div>
+					<div class="form-group">
+						<input type="text" name="pass" value="" class="form-control" placeholder="Password" style="opacity: .67;
+						color:#000" />
+					</div>
+					<span id="unpw_err"></span>
+                    <button class="btn btn-primary login-btn" type="button">Login</button>
+				</div>
+			</div>
+		</div>
+	<!-- </div> -->
+</body>
+</html>
