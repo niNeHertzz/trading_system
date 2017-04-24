28ec88071fc7a24be0a3021ef2789caf57bc5daf
niNeHertzz <glory.marcs@gmail.com>
2017-04-24 12:01:49 +0800

    Changed user interface of category (header)

--- category/update_category_dialog.php
+++ category/update_category_dialog.php
@@ -3,9 +3,15 @@
      <div class="modal-dialog modal-sm vertical-alignment-helper">
          <div class="vertical-align-center">
             <div class="modal-content">
-                <div class="modal-header toast-info" style="padding-top:10px;padding-bottom:10px;background-color:#2f96b4">
+                <!-- updated bgcolor since 04-20-17 -->
+                <div class="modal-header toast-info" 
+                     style="padding-top:10px;padding-bottom:10px;
+                            background-color:#c1c0c0"> <!--#2f96b4-->
                     <button type="button" class="close" style="margin-top:0px;" data-dismiss="modal">&times;</button>
-                    <h5 class="modal-title" style="color:#ffffff;font-size:18px;padding-left:0px;">Update Category</h5>
+                    <!-- updated color, added font family and font weight since 04-20-17 -->
+                    <h5 class="modal-title" 
+                        style="color:#474646;font-family:calibri;
+                               font-size:18px;font-weight:bold;">Update Category</h5>
                 </div>
                 <div class="modal-body">
                     <div class="form-group" style="margin-left: 10px;">
@@ -17,8 +23,8 @@
                     </div>
                     <div style="text-align: right;margin-right: 15px;">
                         <button id="update-category-save" type="button" class="btn btn-default"
-                            style="color:#ffffff;border:none;border-radius: 3px;
-                                   padding-top:5px;padding-bottom:5px;margin-top: 15px;">
+                                style="color:#ffffff;border:none;border-radius:2px;
+                                       padding-top:5px;padding-bottom:5px;margin-top:15px;">
                             Save
                         </button>
                     </div>
