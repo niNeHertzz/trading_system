6b20d43cc322cce04cda8961080617385e0ed0f5
niNeHertzz <glory.marcs@gmail.com>
2017-04-27 18:41:46 +0800

    Add un text for checking if it does have a value

---
+++ user/update.php
@@ -0,0 +1,12 @@
+<?php
+	if (isset($_POST['lnametxt']) && isset($_POST['fnametxt']) && 
+        isset($_POST['mnametxt']) && isset($_POST['gender']) &&
+        isset($_POST['bdatetxt']) && isset($_POST['addresstxt']) &&
+        isset($_POST['contacttxt']) && isset($_POST['unametxt']) &&
+        isset($_POST['role']) && isset($_POST['un']))
+	{
+		require_once("user_query.php");
+		$user = new User();
+		$user->updateUser();
+	}	
+?>
